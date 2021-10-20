f4e9b2d9f57af0259428dc69b47fbd4991dd9fe8
Matúš Ustohal <matus.ustohal@student.tuke.sk>
2021-10-20 19:10:22 +0200

    message

---
+++ ps1/task_1.c
@@ -0,0 +1,78 @@
+#include <superkarel.h>
+    
+void turn_right();
+void skakanie();
+void naspat();
+void kotva();
+
+int main(){
+    turn_on("task_1.kw");
+    
+    set_step_delay(100);
+
+    kotva();
+
+    while(beepers_present()==false){
+        skakanie();
+    }
+
+    pick_beeper();
+
+    while(beepers_present()==false){
+        naspat();
+    }
+    
+    pick_beeper();
+    
+     do{
+        turn_left();
+     }while(not_facing_west());
+
+    turn_off();
+    return 0;
+}
+
+void turn_right(){
+    turn_left();
+    turn_left();
+    turn_left();
+}
+
+    
+void skakanie(){ 
+
+    if(front_is_blocked()){
+        turn_right();
+    }
+
+    if(front_is_clear()){
+        step();
+    }
+    
+    if(right_is_clear()){
+        turn_right();
+    }
+}
+
+void naspat(){
+
+    if(front_is_blocked()){
+        turn_left();
+    }
+
+    if(front_is_clear()){
+        step();
+    }
+    
+    if(left_is_clear()){
+        turn_left();
+    }
+}
+
+void kotva(){
+    while(no_beepers_in_bag()==false){
+        put_beeper();
+    }
+    turn_left();
+    step();
+}
