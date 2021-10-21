db1214ef456cf69fe8ae20bd034c174fe2a4e378
Matúš Ustohal <matus.ustohal@student.tuke.sk>
2021-10-21 18:14:36 +0200

    message

---
+++ ps1/task_3.c
@@ -0,0 +1,110 @@
+#include <superkarel.h>
+
+void turn_around();
+void praca();
+void ukladanie();
+void navrat();
+void turn_right();
+
+int main(){
+
+    turn_on("task_3.kw");
+    set_step_delay(20);
+   
+
+    praca();
+
+
+    turn_off();
+    return 0;
+}
+
+void turn_around(){
+    turn_left();
+    turn_left();
+}
+
+void turn_right(){
+    turn_left();
+    turn_left();
+    turn_left();
+}
+
+void praca(){
+    while(not_facing_south()){
+        turn_left();
+    }
+    if(front_is_clear()){
+    step();
+    }else{
+        navrat();
+    }
+    while(not_facing_east()){
+        turn_left();
+    }
+    while(front_is_clear()){
+        if(beepers_present()){
+            pick_beeper();
+        }
+        step();
+    }
+    if(beepers_present()){
+        pick_beeper();
+    }
+    ukladanie();
+}
+
+void ukladanie(){
+while(beepers_in_bag()){
+   while(not_facing_north()){
+        turn_left();
+    }
+    step();
+    if(beepers_present()){
+        turn_around();
+        step();
+        if(beepers_in_bag()){
+        put_beeper();
+        }
+        turn_right();
+        if(front_is_clear()){
+            step();
+            ukladanie();
+        }else{
+            praca();
+        }
+    }else{
+        turn_around();
+        step();
+        turn_right();
+        if(front_is_clear()){
+            step();
+            ukladanie();
+        }else{
+            praca();
+        }
+    }
+}
+while(not_facing_west()){
+    turn_left();
+}
+while(front_is_clear()){
+    step();
+}
+praca();
+}
+
+void navrat(){
+    while(not_facing_north()){
+        turn_left();
+    }
+    while(front_is_clear()){
+        step();
+    }
+    turn_around();
+    step();
+    while(not_facing_east()){
+        turn_left();
+    }
+    turn_off();
+}
