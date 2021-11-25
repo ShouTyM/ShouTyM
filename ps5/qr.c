7ff596c2d0e51e341b2f6da53776e110a22afe4c
Matúš Ustohal <matus.ustohal@student.tuke.sk>
2021-11-25 16:49:17 +0100

    message

--- ps5/qr.c
+++ ps5/qr.c
@@ -5,6 +5,14 @@
 
 void encode_char(const char character, bool bits[8]);
 
+char decode_byte(const bool bits[8]);
+
+void encode_string(const char string[], bool bytes[strlen(string)+1][8]);
+
+void decode_bytes(const int rows, bool bytes[rows][8], char string[rows]);
+
+void bytes_to_blocks(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8]);
+
 int main(){
     
 /*    bool bits1[8];
@@ -14,6 +22,55 @@ int main(){
         }
         printf("\n");
         // prints: 01000001
+
+    bool bits2[8] = {0,1,0,0,0,0,0,1};
+    printf("%c\n", decode_byte(bits2));
+
+    char* text = "Hello, how are you?";
+    const int len = strlen(text);
+    bool bytes1[len+1][8];
+    encode_string(text, bytes1);
+    for(int j = 0; j <= len; j++){
+        printf("%c: ", text[j]);
+        for(int i = 0; i < 8; i++){
+            printf("%d", bytes1[j][i]);
+        }
+        printf("\n");
+    }
+
+    bool bytes2[7][8] = {
+        {0,1,0,0,1,0,0,0},
+        {0,1,1,0,0,1,0,1},
+        {0,1,1,0,1,1,0,0},
+        {0,1,1,0,1,1,0,0},
+        {0,1,1,0,1,1,1,1},
+        {0,0,1,0,0,0,0,1},
+        {0,0,0,0,0,0,0,0}
+    };
+    char string[7];
+    decode_bytes(7, bytes2, string);
+    printf("%s\n", string);
+
+
+    int length = 4+1, cols = 3, offset = 2;
+    bool bytes1[4+1][8] = {
+        {0,1,0,0,0,0,0,1},
+        {0,1,1,0,1,0,0,0},
+        {0,1,1,0,1,1,1,1},
+        {0,1,1,0,1,0,1,0},
+        {0,0,0,0,0,0,0,0}
+    };
+    bool blocks1[offset*8][cols];
+    bytes_to_blocks(cols, offset, blocks1, length, bytes1);
+    for(int j = 0; j < offset*8; j++){
+        for(int i = 0; i < cols; i++){
+            printf("%d", (blocks1[j][i] == true) ? 1 : 0);
+        }
+        printf("\n");
+        if(j %8 == 7){
+            printf("\n");
+        }
+    }
 */
     return 0;
 }
@@ -30,4 +87,63 @@ void encode_char(const char character, bool bits[8]){
         }
         znak = znak / 2;
     }
+}
+
+char decode_byte(const bool bits[8]){
+    int i;
+    int p = 1;
+    int cislo = 0;
+    int desiatkova = 0;
+    int a = 1;
+    int b;
+    char pismeno;
+    
+    for(i = 7; i >= 0; i--){
+        cislo = cislo + (bits[i] * p);
+
+        p = p * 10;
+    }
+
+    while(cislo != 0){
+        b = cislo %10;
+
+        desiatkova = desiatkova + b * a;
+
+        cislo = cislo / 10;
+
+        a = a * 2;
+    }
+    pismeno = desiatkova;
+
+    return pismeno;
+}
+
+void encode_string(const char string[], bool bytes[strlen(string)+1][8]){
+    int i;
+
+    for(i = 0; i < (strlen(string)+1); i++){
+        encode_char(string[i], bytes[i]);
+    }
+}
+
+void decode_bytes(const int rows, bool bytes[rows][8], char string[rows]){
+    int i;
+
+    for(i = 0; i < rows; i++){
+        string[i] = decode_byte(bytes[i]);
+    }
+}
+
+void bytes_to_blocks(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8]){
+int j = 0;
+int off = offset;
+int stlpec;
+for(off = offset; off>= 0; off--){
+    for(int riadok = 0; riadok < 8; riadok++){
+        j++;
+        for(stlpec = 0; stlpec < rows; stlpec++){
+            blocks[riadok][stlpec] = bytes[stlpec][riadok];
+        }
+    }
+}    
 }
