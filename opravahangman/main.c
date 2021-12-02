de3b6efa9c13f0149d51de563ba4f6e248e84f9f
Matúš Ustohal <matus.ustohal@student.tuke.sk>
2021-12-02 03:30:22 +0100

    message

---
+++ opravahangman/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "hangman.h"
+
+int main(){
+    
+    char secret[15];
+
+    srand(time(NULL));
+
+    if(get_word(secret) == 0){
+        hangman(secret);
+    }
+    return 0;
+}
