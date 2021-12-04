7e081221a31ed039e8bf22aa9dc7adfdc8722bc9
Matúš Ustohal <matus.ustohal@student.tuke.sk>
2021-12-05 00:10:54 +0100

    message

--- ps6/program.c
+++ ps6/program.c
@@ -17,12 +17,15 @@ int main(){
     curs_set(FALSE);
     nodelay(stdscr, TRUE);
     int y = 10, x = 10, a;
+    char b[5];
     int min_y = 9, max_y = 28;
     int min_x = 9, max_x = 78;
     int mapa = 0;
     int moznost = 0;
 
+while(moznost == 0 || moznost == 1 || moznost == 2){
 
+    if(moznost == 0){
     nodelay(stdscr, FALSE);
     echo();
     mvprintw(12, 10, "1. Urovne");
@@ -30,9 +33,8 @@ int main(){
     mvprintw(10, 10, "Vyber si moznost: ");
     refresh();
     scanw("%d", &moznost);
-//    nodelay(stdscr, TRUE);
-  //  noecho();
     clear();
+    }
 
     if(moznost == 1){
         mvprintw(10, 10, "1. Lahke bludisko");
@@ -43,7 +45,10 @@ int main(){
         nodelay(stdscr, TRUE);
         noecho();
         clear();
-    }else if(moznost == 2){    
+        break;
+    }
+    
+    if(moznost == 2){    
         mvprintw(10, 10, "X - Hrac");
         mvprintw(12, 10, "# - Stena");
         mvprintw(14, 10, "* - Bonusove body");
@@ -52,8 +57,23 @@ int main(){
         mvprintw(20, 10, "A - Pohyb dolava");
         mvprintw(22, 10, "S - Pohyb dole");
         mvprintw(24, 10, "D - Pohyb doprava");
-    }
+        mvprintw(10, 10, "Potvrd R aby si sa vratil: ");
+        scanw("%s", b);
 
+        if(b[0] == 'R' || b[0] == 'r'){
+            clear();
+            moznost = 0;
+            continue;
+        }else{
+            clear();
+            mvprintw(10, 10, "Zly vyber!");
+            mvprintw(11,10, "%c",b);
+            refresh();
+            sleep(3);
+            exit(0);
+        }
+    }
+}
 
 
 
@@ -74,7 +94,7 @@ int main(){
         }
     }
 
-    while(1){
+    while(mapa == 1 || mapa == 2 || mapa == 3){
         
         mvprintw(y, x, "O");
         refresh();
