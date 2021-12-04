d9b290f34c2c2c720f65e8a62088ad1cac1f553b
Matúš Ustohal <matus.ustohal@student.tuke.sk>
2021-12-04 23:41:45 +0100

    message

--- ps6/program.c
+++ ps6/program.c
@@ -19,14 +19,57 @@ int main(){
     int y = 10, x = 10, a;
     int min_y = 9, max_y = 28;
     int min_x = 9, max_x = 78;
+    int mapa = 0;
+    int moznost = 0;
+
+
+    nodelay(stdscr, FALSE);
+    echo();
+    mvprintw(12, 10, "1. Urovne");
+    mvprintw(14, 10, "2. Ovladanie/info");
+    mvprintw(10, 10, "Vyber si moznost: ");
+    refresh();
+    scanw("%d", &moznost);
+//    nodelay(stdscr, TRUE);
+  //  noecho();
+    clear();
+
+    if(moznost == 1){
+        mvprintw(10, 10, "1. Lahke bludisko");
+        mvprintw(12, 10, "2. Stredne tazke bludisko");
+        mvprintw(14, 10, "3. Tazke bludisko");
+        mvprintw(8, 10, "Tvoj vyber bludiska: ");
+        scanw("%d", &mapa);
+        nodelay(stdscr, TRUE);
+        noecho();
+        clear();
+    }else if(moznost == 2){    
+        mvprintw(10, 10, "X - Hrac");
+        mvprintw(12, 10, "# - Stena");
+        mvprintw(14, 10, "* - Bonusove body");
+        mvprintw(16, 10, "C - Ciel");
+        mvprintw(18, 10, "W - Pohyb hore");
+        mvprintw(20, 10, "A - Pohyb dolava");
+        mvprintw(22, 10, "S - Pohyb dole");
+        mvprintw(24, 10, "D - Pohyb doprava");
+    }
+
+
+
+
+
+
+
+
 
     //spustenie mapy v
-
-    for(int p = 8; p < 30; p++){
-        for(int o = 8; o < 80; o++){
-            if(o == 8 || o == 79 || p == 8 || p == 29){
-            mvprintw(p, o, "#");
-            refresh();
+    if(mapa == 1){
+        for(int p = 8; p < 30; p++){
+            for(int o = 8; o < 80; o++){
+                if(o == 8 || o == 79 || p == 8 || p == 29){
+                mvprintw(p, o, "#");
+                refresh();
+                }
             }
         }
     }
